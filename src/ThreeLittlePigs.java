7c0adea09dd8bb8634462ee5f344bae3b638cfd8
andiewalker <awalker3@macalester.edu>
2019-12-04 15:39:47 -0600

    Worked on three little pigs so that it has functioning rooms, descriptions, etc. still need to implement other commands like "eat" and "blow down" and talking.

--- src/ThreeLittlePigs.java
+++ src/ThreeLittlePigs.java
@@ -4,29 +4,29 @@ import java.util.List;
 public class ThreeLittlePigs {
     public static void main(String[] args) {
         Room woods = new Room("Woods");
-        woods.setStory("You are a ferocious wolf, hungry, and tired after a long excursion\n " +
-                "out in the deep dark woods. You have just heard tell that there are three little\n " +
-                "pigs who have recently done some construction in the neighborhood…\n");
+        woods.setStory("You are a ferocious wolf, hungry, and tired after a long excursion " +
+                "out in the deep dark woods. You have just heard tell that there are three little " +
+                "pigs who have recently done some construction in the neighborhood…");
         woods.setDescription("These woods are dark and scary.");
 
 
         Room house1 = new Room ("The Straw House");
-        house1.setStory("You’ve trekked out of the woods and followed the rocky dirt road until you\n" +
-                " find a neighborhood full of questionably constructed straw houses. Your nose twitches\n" +
-                "and you realize the one right in front of you quite possibly contains bacon…\n");
+        house1.setStory("You’ve trekked out of the woods and followed the rocky dirt road until you" +
+                " find a neighborhood full of questionably constructed straw houses. Your nose twitches " +
+                "and you realize the one right in front of you quite possibly contains bacon…");
         house1.setDescription("In front of you is a shoddily built straw house.");
 
 
         Room house2 = new Room("The Stick House");
-        house2.setStory("You decide to look for some more food and continue on to the next neighborhood,\n" +
-                " filled with stick houses. You sniff the air and have a good feeling about a house to your left…\n");
+        house2.setStory("You decide to look for some more food and continue on to the next neighborhood," +
+                " filled with stick houses. You sniff the air and have a good feeling about a house to your left…");
         house2.setDescription("Here is the site of another flimsy house.");
 
 
         Room house3 = new Room("The Brick House");
         house3.setStory("You decide to look for some more food and continue on to the next neighborhood, filled with " +
-                "brick houses. You sniff the air and have a good feeling about a house up ahead, which is made of bricks\n" +
-                "and has a lovely ");
+                "brick houses. You sniff the air and have a good feeling about a house up ahead, which is made of bricks" +
+                "and has a lovely skylight");
         house3.setDescription("Here is the site of the brick house. You can see a skylight.");
 
 
@@ -55,15 +55,11 @@ public class ThreeLittlePigs {
         house3.setConnections(new Room[] {null, null, null, house2, house3Interior, null});
         house3Interior.setConnections(new Room[] {null, null, null, null, null, house3});
 
-        ArrayList<String> commands = new ArrayList<>(List.of("go", "show inventory", "pick up", "drop", "open"));
+        ArrayList<String> commands = new ArrayList<>(List.of("go", "show inventory", "pick up", "drop", "open", "eat"));
         TextAdventure threePigs = new TextAdventure("", commands,
                 new ArrayList<> (List.of(woods, house1, house2, house3, house3Interior)));
         threePigs.startGame();
         threePigs.runGame();
-
-
-
-
 
     }
 }
