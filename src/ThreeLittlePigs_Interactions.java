7ae249f6941732161703f350d46cac40267cc7ec
ariadne-smith <asmith8@macalester.edu>
2019-12-08 15:15:26 -0600

    changed nothing

---
+++ src/ThreeLittlePigs_Interactions.java
@@ -0,0 +1,21 @@
+public class ThreeLittlePigs_Interactions {
+
+    /*
+    item interaction methods -- must be static
+     */
+
+    static void doNothing(){
+        System.out.println("You can't do that.");
+    }
+
+    static void breakWindow(){
+
+    }
+
+    //brick & window
+    //leafblower & stick house
+    //blow command
+    //eat command
+    //use _ with _
+
+}
